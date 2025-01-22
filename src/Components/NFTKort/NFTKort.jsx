cd4bb56abfbed05a8a64070d63eb9c1076577919
Torjus <Torjus24sfj@gmail.com>
2025-01-22 11:05:08 +0100

    css styling

--- src/Components/NFTKort/NFTKort.jsx
+++ src/Components/NFTKort/NFTKort.jsx
@@ -6,15 +6,35 @@ export function NFTKort() {
       <section className={style.nft_card_section}>
         <div className={style.card_container}>
           <div>
-            <img className={style.main_image} src="/src/assets/images/NFTKort/image-equilibrium.jpg" alt="" />
+            <img
+              className={style.main_image}
+              src="/src/assets/images/NFTKort/image-equilibrium.jpg"
+              alt=""
+            />
           </div>
           <div>
             <h1>Equilibrium</h1>
             <h3>Our Equilibrium collection promotes balance and calm</h3>
           </div>
           <div className={style.etherium}>
-            <img src="/src/assets/images/NFTKort/icon-ethereum.svg" alt="" /><span>0.041 ETH</span><img src="/src/assets/images/NFTKort/icon-clock.svg" alt="" /><span>3 days left</span>
+            <div className={style.etherium_icon_container}>
+              <img
+                className={style.etherium_icon}
+                src="/src/assets/images/NFTKort/icon-ethereum.svg"
+                alt=""
+              />
+              <span className={style.cyan}>0.041 ETH</span>
+            </div>
+            <div className={style.etherium_icon_container}>
+              <img
+                className={style.clock_icon}
+                src="/src/assets/images/NFTKort/icon-clock.svg"
+                alt=""
+              />
+              <span  className={style.gray}>3 days left</span>
+            </div>
           </div>
+          <span className={style.devider}></span>
         </div>
       </section>
     </>
