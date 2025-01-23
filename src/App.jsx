c5b3ca2179f51a65f81ba87cf4cee6f249fd32ac
Torjus <Torjus24sfj@gmail.com>
2025-01-23 10:49:58 +0100

    some shit yo

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import "./App.css";
-import { MatKort } from "./Components/MatKort/MatKort";
-import { NFTKort } from "./Components/NFTKort/NFTKort";
-import { ResultatOversikt } from "./Components/ResultatOversikt/ResultatOversikt";
+import { BlogPreview } from "./Components/BlogPreview/BlogPreview";
+// import { MatKort } from "./Components/MatKort/MatKort";
+// import { NFTKort } from "./Components/NFTKort/NFTKort";
+// import { ResultatOversikt } from "./Components/ResultatOversikt/ResultatOversikt";
 
 function App() {
   return (
     <>
       <div className="gallery">
-        <div className="component">
+        {/* <div className="component">
           <MatKort />
-        </div>
-        <div className="component">
+        </div> */}
+        {/* <div className="component">
           <NFTKort />
-        </div>
-        <div className="component">
+        </div> */}
+        {/* <div className="component">
           <ResultatOversikt />
+        </div> */}
+        <div className="component">
+          <BlogPreview />
         </div>
       </div>
     </>
---
+++ src/Components/BlogPreview/BlogPreview.jsx
@@ -0,0 +1,9 @@
+export function BlogPreview() {
+    return (
+        <>
+        <div>
+            <h1>Hello</h1>
+        </div>
+        </>
+    )
+}
