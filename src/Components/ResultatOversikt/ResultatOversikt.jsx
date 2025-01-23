66a4ee9c3b54151f183e0aefb543453a90233a61
Torjus <Torjus24sfj@gmail.com>
2025-01-23 09:50:19 +0100

    remove useless code

--- src/Components/ResultatOversikt/ResultatOversikt.jsx
+++ src/Components/ResultatOversikt/ResultatOversikt.jsx
@@ -15,7 +15,7 @@ export function ResultatOversikt() {
       <section className={style.results_section}>
         <div className={style.card_container}>
           <div className={style.card}>
-            <div className={style.card_content_l}>
+            <div className={`${style.card_content_l} flex-column`}>
               <p className={style.result}>Your Result</p>
               <div className={style.circle}>
                 <h2 className={style.head_count}>{score.totalscore}</h2>
@@ -32,37 +32,37 @@ export function ResultatOversikt() {
             <div className={style.card_content_r}>
                 <h2 className={style.summary}>Summary</h2>
               <div className={style.rounded}>
-                <div className={style.round_red}>
+                <div className={`${style.round_base} ${style.round_red}`}>
                   <img className={style.round_img} 
                     src="/src/assets/images/ResultatOversikt/icon-reaction.svg"
                     alt=""
                     />
                   <p>Reaction</p>
-                  <p className={style.score_rounded}>80 / 100</p>
+                  <p className={style.score_rounded_text}>{score.reaction}<span> / 100</span></p>
                 </div>
-                <div className={style.round_yellow}>
+                <div className={`${style.round_base} ${style.round_yellow}`}>
                   <img className={style.round_img} 
                     src="/src/assets/images/ResultatOversikt/icon-memory.svg"
                     alt=""
                   />
                   <p>Memory</p>
-                  <p className={style.score_rounded}>92 / 100</p>
+                  <p className={style.score_rounded_text}>{score.memory}<span> / 100</span></p>
                 </div>
-                <div className={style.round_green}>
+                <div className={`${style.round_base} ${style.round_green}`}>
                   <img className={style.round_img} 
                     src="/src/assets/images/ResultatOversikt/icon-verbal.svg"
                     alt=""
                   />
                   <p>Verbal</p>
-                  <p  className={style.score_rounded}>61 / 100</p>
+                  <p  className={style.score_rounded_text}>{score.verbal}<span> / 100</span></p>
                 </div>
-                <div className={style.round_blue}>
+                <div className={`${style.round_base} ${style.round_blue}`}>
                   <img className={style.round_img} 
                     src="/src/assets/images/ResultatOversikt/icon-visual.svg"
                     alt=""
                   />
                   <p>Visual</p>
-                  <p className={style.score_rounded}>73 / 100</p>
+                  <p className={style.score_rounded_text}>{score.visual}<span> / 100</span></p>
                 </div>
                 <div className={style.btn}>Continue</div>
               </div>
