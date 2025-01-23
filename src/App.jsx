8d28e5c37edd7b98cc60f3ecfbe891a33940026f
Torjus <Torjus24sfj@gmail.com>
2025-01-23 10:31:30 +0100

    App css temp

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import './App.css'
-// import { MatKort } from './Components/MatKort/MatKort'
-// import { NFTKort } from './Components/NFTKort/NFTKort'
-import { ResultatOversikt } from './Components/ResultatOversikt/ResultatOversikt'
+import "./App.css";
+import { MatKort } from "./Components/MatKort/MatKort";
+import { NFTKort } from "./Components/NFTKort/NFTKort";
+import { ResultatOversikt } from "./Components/ResultatOversikt/ResultatOversikt";
 
 function App() {
-
   return (
     <>
-    <div className="gallery">
-      {/* <MatKort /> */}
-      {/* <NFTKort /> */}
-      <ResultatOversikt />
-    </div>
+      <div className="gallery">
+        <div className="component">
+          <MatKort />
+        </div>
+        <div className="component">
+          <NFTKort />
+        </div>
+        <div className="component">
+          <ResultatOversikt />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
