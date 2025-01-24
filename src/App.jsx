215610e2f21c0e18ed8203c67f9aa39d29c27cd9
Torjus Tveten <55440508+Torjustveten@users.noreply.github.com>
2025-01-24 12:26:44 +0100

    app

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,6 @@ import { BlogPreview } from "./Components/BlogPreview/BlogPreview";
 import { MatKort } from "./Components/MatKort/MatKort";
 import { NFTKort } from "./Components/NFTKort/NFTKort";
 import { ResultatOversikt } from "./Components/ResultatOversikt/ResultatOversikt";
-// import { MatKort } from "./Components/MatKort/MatKort";
-// import { NFTKort } from "./Components/NFTKort/NFTKort";
-// import { ResultatOversikt } from "./Components/ResultatOversikt/ResultatOversikt";
 import SocialKort from "./Components/SocialKort/SocialKort";
 
 function App() {
