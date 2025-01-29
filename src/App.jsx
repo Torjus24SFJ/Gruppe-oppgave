e47fcc1d6e2ab1f587e047603e3ee2f3b0340056
Torjus <Torjus24sfj@gmail.com>
2025-01-29 09:18:22 +0100

    fixing export

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import "./App.css";
 import { AdviceGenerator } from "./Components/AdviceGenerator/AdviceGenerator";
-// import { BlogPreview } from "./Components/BlogPreview/BlogPreview";
-// import { MatKort } from "./Components/MatKort/MatKort";
-// import { NFTKort } from "./Components/NFTKort/NFTKort";
-// import {QRCodeComponent} from "./Components/QR-codeComponent/qr-code-component-main/assets/QRCodeComponent";
-// import { ResultatOversikt } from "./Components/ResultatOversikt/ResultatOversikt";
-// import SocialKort from "./Components/SocialKort/SocialKort";
+import { BlogPreview } from "./Components/BlogPreview/BlogPreview";
+import { MatKort } from "./Components/MatKort/MatKort";
+import { NFTKort } from "./Components/NFTKort/NFTKort";
+import {QRCodeComponent} from "./Components/QR-codeComponent/QRCodeComponent";
+import { ResultatOversikt } from "./Components/ResultatOversikt/ResultatOversikt";
+import SocialKort from "./Components/SocialKort/SocialKort";
 
 function App() {
   return (
     <>
       <main>
         <div className="gallery">
-          {/* <div className="component">
+          <div className="component">
             <MatKort />
           </div>
           <div className="component">
@@ -29,9 +29,12 @@ function App() {
           </div>
           <div className="component">
             <QRCodeComponent />
-          </div> */}
+          </div>
           <div className="component">
             <AdviceGenerator />
+          </div>
+          <div className="component">
+            <QRCodeComponent />
           </div>
         </div>
       </main>
--- src/Components/QR-codeComponent/QRCodeComponent.jsx
+++ src/Components/QR-codeComponent/QRCodeComponent.jsx
@@ -1,9 +1,7 @@
-//*import React from "react";
-
 import "./QRCodeComponent.css";
 import qrCodeImage from "./assets/image-qr-code.png";
 
-const QRCodeComponent = () => {
+export const QRCodeComponent = () => {
   return (
     <div className="qr-card">
       <img src={qrCodeImage} alt="QR Code" className="qr-image" />
@@ -18,4 +16,3 @@ const QRCodeComponent = () => {
   );
 };
 
-export default QRCodeComponent;
